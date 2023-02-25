58b5bf894728a09e64ec975ed2a49b10281df8fc
MakesYT <42534870+MakesYT@users.noreply.github.com>
2023-02-25 12:07:02 +0800

    清理

--- src/main/java/top/ncserver/chatimg/Tools/ImgSendTool.java
+++
@@ -1,4 +0,0 @@
-package top.ncserver.chatimg.Tools;
-
-public class ImgSendTool {
-}
--- src/main/java/top/ncserver/chatimg/Tools/event/KeyBoardInput.java
+++
@@ -1,9 +0,0 @@
-package top.ncserver.chatimg.Tools.event;
-
-import net.minecraftforge.api.distmarker.Dist;
-import net.minecraftforge.fml.common.Mod;
-
-@Mod.EventBusSubscriber(value = Dist.CLIENT)
-public class KeyBoardInput {
-
-}
