ed02d8b8874961412f86b378408728bb08cb4777
MakesYT <42534870+MakesYT@users.noreply.github.com>
2023-03-15 11:40:22 +0800

    尝试修复mixin错误

--- src/main/java/top/ncserver/chatimg/Tools/mixin/ChatScreen.java
+++ src/main/java/top/ncserver/chatimg/Tools/mixin/ChatScreen.java
@@ -3,15 +3,12 @@ package top.ncserver.chatimg.Tools.mixin;
 import net.minecraft.client.gui.CommandSuggestionHelper;
 import net.minecraft.client.gui.chat.NarratorChatListener;
 import net.minecraft.client.gui.screen.Screen;
-import net.minecraft.client.gui.widget.TextFieldWidget;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Overwrite;
 import org.spongepowered.asm.mixin.Shadow;
 
 @Mixin(net.minecraft.client.gui.screen.ChatScreen.class)
 public abstract class ChatScreen extends Screen {
-    @Shadow
-    protected TextFieldWidget input;
     @Shadow
     private CommandSuggestionHelper commandSuggestions;
     @Shadow
